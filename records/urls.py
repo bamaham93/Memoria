19e3565e059d594c821c42bdfccbf8a2ae8c63b2
Jacob McGowin <bamaham93@gmail.com>
2025-08-26 01:29:32 -0400

    Added test doc, basic views, etc.

--- records/urls.py
+++ records/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+# in records/urls.py
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path("", views.document_list, name="document_list"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="records/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
--- records/views.py
+++ records/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Document
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 def document_list(request):
     docs = Document.objects.all().order_by("-uploaded_at")
-    return render(request, "document_list.html", {"docs": docs})
+    return render(request, "records/document_list.html", {"docs": docs})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect("login")  # after signup, go to login
+    else:
+        form = UserCreationForm()
+    return render(request, "records/signup.html", {"form": form})
